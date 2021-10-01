a37718bd34e9eb3deaa4530a266783bedd252c02
Jason-Cee <jasoncee017@gmail.com>
2021-10-01 16:27:02 +0200

    Backend done

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config')
+const { check, validationResult } = require('express-validator/check');
 
-const bcrypt = require('bcryptjs')
-const { check, validationResult } = require('express-validator/check')
-
-const User = require('../models/User')
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
@@ -44,8 +45,18 @@ router.post('/',
 
         await user.save();
 
-        res.send('User Saved');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
+        jwt.sign(payload, config.get('jwtSecret'), {
+             expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({ token })
+        })
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error')
